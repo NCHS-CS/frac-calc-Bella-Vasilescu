92b363f87b7e8f13b9702dc8520661625cd54df1
Bella-Vasilescu <2001662@apps.nsd.org>
2025-12-02 21:45:55 +0000

    mixed not proper

--- FracCalc.java
+++ FracCalc.java
@@ -91,7 +91,7 @@ public class FracCalc {
    public static String provideHelp() {
       String help = "This program performs arithmetic (+,-,/,*) operations.\n";
       help += "Enter your expression in the format \"Fraction operator fraction\". For example, 2/3 + 4/5.\n";
-      help += "To create proper fractions, place a '_' where there is a space. For example, 1_1/2 (3/2).";
+      help += "To create mixed fractions, place a '_' where there is a space. For example, 1_1/2 (3/2).";
       
       return help;
    }
